6c4b481579fc9dd33b025b50c7d72a170501c8e2
guilherme <mercante243@gmail.com>
2021-01-04 10:25:59 -0300

    ajustes para o mobile

--- public/js/jquery/validaLogin.js
+++ public/js/jquery/validaLogin.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
-    $("#login").click(function(){
+    $("#btnLogin").click(function(){
         var displaySucess = $("#alertSucess")
         var displayFail = $("#alertFail")
+        var login = $("#login")
         if($("#email").val() == "" || $("#password").val() == ""){
             $(displaySucess).css("display", "none")
             $(displayFail).css("display", "block")
+            $(login).css("margin-top", "0")
         }
     });   
     
-    $("#login").click(function (){
+    $("#btnLogin").click(function (){
         var displaySucess = $("#alertSucess")
         var displayFail = $("#alertFail")
+        var login = $("#login")
         if($("#email").val() != "" && $("#password").val() != "")  {
             $(displaySucess).css("display", "block")
             $(displayFail).css("display", "none")
+            $(login).css("margin-top", "0")
             
         }
 
